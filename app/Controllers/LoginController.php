c69175b24ff22b8761c46808f20e3500be60367d
Jean Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2019-03-03 23:23:59 -0500

    Se empieza la creacion de la pagina Perfil y se crea una redireccion en el login

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -1,28 +1,43 @@
 <?php
 class LoginController extends Controller
 {
+  function __construct()
+  {
+  }
+
   public function formulario()
   {
+    if (isset($_SESSION['md5'])) {
+      header("Location: /");
+    }
     self::setLayout("Auth");
     self::vista("Auth/Login");
   }
 
   public function iniciar()
   {
-    $u = self::modelo("Usuario");
-
     $codigo = Request::post("codigo");
     $pass =  Request::post("password");
 
-    $usuario = $u->getUsuario($codigo);
+    $query = DB::query(
+      "SELECT usuarios.nombre,usuarios.apellido,usuarios.codigo,usuarios.password,usuarios.correo,usuarios.documento_identidad,roles.nombre AS rol
+       FROM usuarios
+       INNER JOIN roles
+       ON usuarios.rol = roles.id
+       WHERE codigo = '$codigo'");
+
+    $usuario = DB::single($query);
 
     if (count($usuario) >= 1 && password_verify($pass,$usuario['password'])) {
       $md5 = md5(time().$codigo);
+
       $_SESSION['md5'] = $md5;
       $_SESSION['id'] = $usuario['id'];
       $_SESSION['nombre'] = $usuario['nombre'];
       $_SESSION['apellido'] = $usuario['apellido'];
       $_SESSION['codigo'] = $usuario['codigo'];
+      $_SESSION['correo'] = $usuario['correo'];
+      $_SESSION['di'] = $usuario['documento_identidad'];
       $_SESSION['rol'] = $usuario['rol'];
 
       header("Location: /");
--- app/Views/Perfil.php
+++ app/Views/Perfil.php
@@ -1,4 +1,45 @@
 <title>Perfil</title>
   <div class="container my-3">
+  <div class="row">
+    <!-- Datos Perfil -->
+    <div class="col-md-4">
+      <div class="card card-body flex-center">
+          <div class="row flex-center pb-4">
+              <img class="img-perfil border-radius" src="imgs/default_user.png" alt="Foto de <?= "{$_SESSION["nombre"]} {$_SESSION["apellido"]}" ?>">
+          </div>
 
+          <small class="text-muted"><?= $_SESSION["rol"] ?></small>
+          <h3 class="h3-responsive font-weight-bold"><?= "{$_SESSION["nombre"]} {$_SESSION["apellido"]}" ?></h3>
+          <p><?= $_SESSION["correo"] ?></p>
+          <nav class="nav py-2">
+            <div class="nav-item">
+              <a href="#" class="nav-link">Editar Perfil</a>
+            </div>
+            <div class="nav-item">
+              <a href="#" class="nav-link">Cambiar contraseña</a>
+            </div>
+          </nav>
+      </div>
+    </div>
+    <!-- Datos Perfil -->
+    <!-- Asignaturas del Docente -->
+    <div class="col-md-8 my-auto py-2">
+      <div class="card card-body">
+        <div class="card-title">
+          <h3>Tus Asignaturas</h3>
+          <p>Cuentas con <b>X</b> Asignaturas.</p>
+        </div>
+        <div class="px-3">
+          <ul class="list-group list-group-flush">
+            <?php for ($i = 1;$i<=5;$i++): ?>
+              <li class="list-group-item">
+                <a href="#">04-12312M-50 <b>Asignatura <?= $i ?></b></a>
+              </li>
+            <?php endfor; ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Asignaturas del Docente -->
+  </div>
 </div>
