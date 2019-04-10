f5cce9c76f23d7f182006de3633799b5b3c10276
Jean Gallego <gallegomosquerajeancarlos@gmail.com>
2019-04-10 15:11:13 -0500

    Continuan los cambios a 'Perfil de Usuarios'

--- resources/views/admin/secretario/show.blade.php
+++ resources/views/admin/secretario/show.blade.php
@@ -3,32 +3,39 @@
 @section('title',$user->nombre_completo())
 
 @section('content')
+
 <div class="container">
 	<div class="row align-items-center">
 		<div class="col-md-12 col-lg-10 mx-auto">
-			<div class="card card-body align-items-center">
-				<div class="imagen-perfil">
-					<img src="{{ asset('/images/default_user.png')}}" alt="Foto de {{$user->nombre_completo()}}" class="imagen-peril--img">
+			<div class="row">
+				<div class="col-md-4 d-flex align-items-stretch">
+					<div class="card card-body">
+						<div class="text-center">
+							<img src="{{ asset('/images/default_user.png')}}" alt="Foto de {{$user->nombre_completo()}}" class="img-perfil">
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="card card-body">
-				<small>{{ $user->getRoleNames()[0]}}</small>
-				<h2 class="h2-responsive font-weight-bold">
-					{{$user->nombre}}
-				</h2>
-				<h5 class="h5-responsive text-muted">
-					{{$user->apellido}}
-				</h5>
-				<hr>
-				<p>Último inicio de sesión {{ now() }}</p>
-				<ul class="nav mx-auto font-weight-bold">
-					<li class="nav-item">
+				<div class="col-md-8 d-flex align-items-stretch">
+					<div class="card card-body my-auto">
+						<small>{{ $user->getRoleNames()[0]}}</small>
+						<h2 class="h2-responsive font-weight-bold">
+							{{$user->nombre}}
+						</h2>
+						<h5 class="h5-responsive text-muted">
+							{{$user->apellido}}
+						</h5>
+						<hr>
+						<p>Último inicio de sesión {{ now() }}</p>
+						<ul class="nav mx-auto font-weight-bold text-center">
+							<li class="nav-item">
 						<a href="{{ route('secretarios.edit',$user) }}" class="nav-link btn btn-info"> <i class="fa fa-pen"></i> Editar</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" class="nav-link btn btn-danger"> <i class="fa fa-trash"></i> Desactivar Usuario</a>
-					</li>
-				</ul>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link btn btn-danger"> <i class="fa fa-trash"></i> Desactivar Usuario</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 			<div class="card card-body">
 				<small class="text-muted">Correo electrónico</small>
@@ -38,5 +45,6 @@
 			</div>
 		</div>
 	</div>
-	<div>	
-		@endsection
+	<!-- Fin Datos del Usuario -->
+</div>
+@endsection
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -9,14 +9,14 @@
 	<div class="row align-items-center">
 		<div class="col-md-12 col-lg-10 mx-auto">
 			<div class="row">
-				<div class="col-md-4 d-flex align-items-stretch">
+				<div class="col-md-12 col-lg-4 d-flex align-items-stretch">
 					<div class="card card-body">
 						<div class="text-center">
 							<img src="{{ asset('/images/default_user.png')}}" alt="Foto de {{auth()->user()->nombre_completo()}}" class="img-perfil">
 						</div>
 					</div>
 				</div>
-				<div class="col-md-8 d-flex align-items-stretch">
+				<div class="col-md-12 col-lg-8 d-flex align-items-stretch">
 					<div class="card card-body my-auto">
 						<small>{{ auth()->user()->getRoleNames()[0]}}</small>
 						<h2 class="h2-responsive font-weight-bold">
@@ -46,40 +46,44 @@
 			</div>
 		</div>
 	</div>
-	<div>	
-		<!-- Fin Datos del Usuario -->
-
-
-		@hasrole('docente')
-		<!-- Clases para Hoy -->
-		<div class="my-auto py-2">
-			<div class="card card-body">
-				<small class="text-muted pb-2">Clases para hoy</small>
-				<div class="pl-2">
-					<p class="m-0 text-muted">Dia</p>
-					<h4 class="m-0 h4-responsive font-weight-bold">Nombre Asignatura</h4>
-					<p class="m-0">Hora</p>
-				</div>
-			</div>
-		</div>
-		<!-- Fin Clases para hoy -->
-		<!-- Asignaturas del Docente -->
-		<div class="my-auto py-2">
-			<div class="card card-body">
-				<small class="text-muted pb-2">Tus asignaturas</small>
-				<div class="pl-2">
-					<ul class="list-group list-group-flush">
-						<?php for($i=1;$i<=5;$i++): ?>
-						<li class="list-group-item"><a href="#">4-12314M-50 INTRODUCCIÓN A LOS
-							PÁJAROS-<?= $i  ?></a></li>
-							<?php endfor; ?>
-						</ul>
+	<!-- Fin Datos del Usuario -->
+	@hasrole('Docente')
+	<div class="row">
+		<div class="col-md-12 col-lg-10 mx-auto">
+			<!-- Clases para Hoy -->
+			<div class="my-auto py-2">
+				<div class="card card-body">
+					<small class="text-muted pb-2">Clases para hoy</small>
+					<div class="pl-2">
+						<p class="m-0 text-muted">Dia</p>
+						<h4 class="m-0 h4-responsive font-weight-bold">Nombre Asignatura</h4>
+						<p class="m-0">Hora</p>
 					</div>
 				</div>
 			</div>
-			<!-- Fin Asignaturas del Docente -->
-			@endhasrole
 		</div>
 	</div>
-</div>
-@endsection
+	<!-- Fin Clases para hoy -->
+	<div class="row">
+		<div class="col-md-12 col-lg-10 mx-auto">
+			<!-- Asignaturas del Docente -->
+			<div class="my-auto py-2">
+				<div class="card card-body">
+					<small class="text-muted pb-2">Tus asignaturas</small>
+					<div class="pl-2">
+						<ul class="list-group list-group-flush">
+							<?php for($i=1;$i<=5;$i++): ?>
+							<li class="list-group-item"><a href="#">4-12314M-50 INTRODUCCIÓN A LOS
+								PÁJAROS-<?= $i  ?></a></li>
+								<?php endfor; ?>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- Fin Asignaturas del Docente -->
+			</div>
+		</div>
+		@endhasrole
+	</div>
+	@endsection
+	
