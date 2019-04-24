b51b64cae4dceee8831fed3caacf53ca710d64b1
juinerS <38709520+juinerS@users.noreply.github.com>
2019-04-23 19:38:10 -0500

    g

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function descargarPDF(){
+        $pdf = \PDF::loadView("pdf");
+        return $pdf->download("planeador.pdf");
+    }
 }
